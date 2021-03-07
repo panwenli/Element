b424361b545fb9ebf847eb0cf1adada06368dd1a
你的用户名 <517967304@qq.com>
2021-03-07 10:30:48 +0800

    第一次提交

---
+++ src/form/index.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="app">
+    <div>
+      <el-radio v-model="radio" label="1">备选项</el-radio>
+      <el-radio v-model="radio" label="2">备选项</el-radio>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <el-radio disabled v-model="radio1" label="禁用">备选项</el-radio>
+      <el-radio disabled v-model="radio1" label="选中且禁用">备选项</el-radio>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <el-radio-group v-model="radio2">
+        <el-radio :label="3">备选项</el-radio>
+        <el-radio :label="6">备选项</el-radio>
+        <el-radio :label="9">备选项</el-radio>
+      </el-radio-group>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <div>
+        <el-radio-group v-model="radioa">
+          <el-radio-button label="上海"></el-radio-button>
+          <el-radio-button label="北京"></el-radio-button>
+          <el-radio-button label="广州"></el-radio-button>
+          <el-radio-button label="深圳"></el-radio-button>
+        </el-radio-group>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio-group v-model="radiob" size="medium">
+          <el-radio-button label="上海"></el-radio-button>
+          <el-radio-button label="北京"></el-radio-button>
+          <el-radio-button label="广州"></el-radio-button>
+          <el-radio-button label="深圳"></el-radio-button>
+        </el-radio-group>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio-group v-model="radioc" size="small">
+          <el-radio-button label="上海"></el-radio-button>
+          <el-radio-button label="北京" disabled></el-radio-button>
+          <el-radio-button label="广州"></el-radio-button>
+          <el-radio-button label="深圳"></el-radio-button>
+        </el-radio-group>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio-group v-model="radiod" disabled size="mini">
+          <el-radio-button label="上海"></el-radio-button>
+          <el-radio-button label="北京"></el-radio-button>
+          <el-radio-button label="广州"></el-radio-button>
+          <el-radio-button label="深圳"></el-radio-button>
+        </el-radio-group>
+      </div>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <div>
+        <el-radio v-model="radio3" label="1" border>备选项1</el-radio>
+        <el-radio v-model="radio3" label="2" border>备选项2</el-radio>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio v-model="radio4" label="1" border size="medium"
+          >备选项1</el-radio
+        >
+        <el-radio v-model="radio4" label="2" border size="medium"
+          >备选项2</el-radio
+        >
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio-group v-model="radio5" size="small">
+          <el-radio label="1" border>备选项1</el-radio>
+          <el-radio label="2" border disabled>备选项2</el-radio>
+        </el-radio-group>
+      </div>
+      <div style="margin-top: 20px">
+        <el-radio-group v-model="radio6" size="mini" disabled>
+          <el-radio label="1" border>备选项1</el-radio>
+          <el-radio label="2" border>备选项2</el-radio>
+        </el-radio-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      radio: "1",
+      radio1: "选中且禁用",
+      radio2: 3,
+      radioa: "上海",
+      radiob: "上海",
+      radioc: "上海",
+      radiod: "上海",
+      radio3: "1",
+      radio4: "1",
+      radio5: "1",
+      radio6: "1",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/form/index2.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <div>
+      <el-input v-model="input" placeholder="请输入内容"></el-input>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <el-input
+        placeholder="请输入内容"
+        v-model="input1"
+        :disabled="true"
+      ></el-input>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <el-input placeholder="请输入内容" v-model="input2" clearable> </el-input>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <el-input
+        placeholder="请输入密码"
+        v-model="input3"
+        show-password
+      ></el-input>
+    </div>
+    <el-divider></el-divider>
+    <div>
+      <div class="demo-input-suffix">
+        属性方式:
+        <el-input
+          placeholder="请输入日期"
+          suffix-icon="el-icon-data"
+          v-model="input4"
+        ></el-input>
+        <el-input
+          placeholder="请输入内容"
+          prefix-icon="el-cion-search"
+          v-model="input5"
+        ></el-input>
+      </div>
+      <div class="demo-input-suffix">
+        solt 方式:
+        <el-input placeholder="请选择日期" v-model="input6">
+          <i slot="suffix" class="el-input_icon el-icon-date"></i>
+        </el-input>
+        <el-input placeholder="请输入内容" v-model="input7">
+            <i slot="prefix" class="el-input_icon el-icon-search"></i>
+        </el-input>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+      input1: "",
+      input2: "",
+      input3: "",
+      input4: "",
+      input5: "",
+      input6: "",
+      input7: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
